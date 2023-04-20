7a29a99c1e162a08a989fd6aebac95d16f860867
natividadesusana <susanajdsn@gmail.com>
2023-04-20 13:58:51 -0300

    operations listing

--- src/app.js
+++ src/app.js
@@ -6,13 +6,16 @@ import db from "./database/db.js";
 import { v4 as uuid } from "uuid";
 import Joi from "joi";
 
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 dotenv.config();
 
+
 const ENCRYPTION_ROUNDS = 10;
+
 
 const registrationSchema = Joi.object({
   name: Joi.string().required(),
@@ -20,10 +23,12 @@ const registrationSchema = Joi.object({
   password: Joi.string().min(3).required(),
 });
 
+
 const signInValidationSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(3).required(),
 });
+
 
 app.post("/cadastro", async (req, res) => {
   const { name, email, password } = req.body;
@@ -54,6 +59,7 @@ app.post("/cadastro", async (req, res) => {
   return res.status(201).json({ message: "Usuário criado com sucesso!" });
 });
 
+
 app.post("/", async (req, res) => {
   const { email, password } = req.body;
 
@@ -83,6 +89,7 @@ app.post("/", async (req, res) => {
 
   res.status(200).json({ token });
 });
+
 
 app.post("/nova-transacao/:tipo", async (req, res) => {
   const { tipo } = req.params;
@@ -131,7 +138,54 @@ app.post("/nova-transacao/:tipo", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+
+app.get("/home", async (req, res) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Não autorizado!" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+    if (!session) {
+      return res.status(401).json({ message: "Token inválido!" });
+    }
+
+    const user = await db.collection("users").findOne({ _id: session.userId });
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado!" });
+    }
+
+    const transactions = await db
+      .collection("transacoes")
+      .find({ userId: session.userId })
+      .sort({ createdAt: -1 })
+      .toArray();
+
+    const balance = transactions.reduce((accumulator, transaction) => {
+      if (transaction.type === "entrada") {
+        accumulator += transaction.value;
+      } else {
+        accumulator -= transaction.value;
+      }
+      return accumulator;
+    }, 0);
+
+    return res.status(200).json({ user: user.name, transactions, balance });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ message: "Ocorreu um erro ao listar as transações!" });
+  }
+});
+
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
