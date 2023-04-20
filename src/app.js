73437846020659ad6d3ff2c7a9c306b051da3d53
natividadesusana <susanajdsn@gmail.com>
2023-04-20 02:26:55 -0300

    login route /

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import db from "./database/db.js";
+import { v4 as uuid } from "uuid";
+import Joi from "joi";
+
 
 const app = express();
 
@@ -10,7 +13,15 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 
+
 const ENCRYPTION_ROUNDS = 10;
+
+
+const signInValidationSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(3).required(),
+});
+
 
 app.post("/cadastro", async (req, res) => {
   const { name, email, password } = req.body;
@@ -48,7 +59,37 @@ app.post("/cadastro", async (req, res) => {
   return res.status(201).json({ message: "Usuário criado com sucesso!" });
 });
 
-const PORT = process.env.PORT || 3100;
+
+app.post("/", async (req, res) => {
+  const { email, password } = req.body;
+
+  const validation = signInValidationSchema.validate(req.body, { abortEarly: false });
+  if (validation.error)
+    return res
+      .status(422)
+      .send(validation.error.details.map((detail) => detail.message));
+
+  const user = await db.collection("users").findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ message: "Usuário não encontrado!" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Senha incorreta!" });
+  }
+
+  const token = uuid();
+
+  await db.collection("sessions").insertOne({ token, userId: user._id });
+
+  res.status(200).json({ token });
+});
+
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
